763bacba78d2c186cdda558263dc00830ff32810
Adhithyan <Adhithyan@DESKTOP-NIUOQBB>
2025-08-06 15:28:57 +0530

    This is my java program

---
+++ C2TC_CoreJavaProgram/src/day21/userdefinedannotation/AndroidSeries.java
@@ -0,0 +1,15 @@
+package day21.userdefinedannotation;
+
+@SmartTV(os = "android", height = 40, width = 50)
+@SmartPhone()
+
+public class AndroidSeries {
+
+	String model;
+	int screenSize;
+
+	public AndroidSeries(String model, int screenSize) {
+		this.model = model;
+		this.screenSize = screenSize;
+	}
+}
